4745b8989888f8d539a90bf1a34c08b4f2cb0521
Nicolai Sattler <N.Sattler@bkr.nl>
2021-10-28 14:12:22 +0200

    wip service bus

---
+++ TrainingAZ204_ServiceBus/Program.cs
@@ -0,0 +1,56 @@
+﻿using Azure.Messaging.ServiceBus;
+using Microsoft.Azure.ServiceBus.Core;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Threading.Tasks;
+
+namespace TrainingAZ204_ServiceBus
+{
+    class Program
+    {
+        protected IConfiguration Configuration { get; set; }
+
+        private const string ArjenTopicName = "arjen";
+        private const string ConnectionString = "Endpoint=sb://johngortersbns.servicebus.windows.net/;TransportType=AmqpWebSockets;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=ClDhfDCAdWbTRQyboJkXcSCf1orZNsvblxy6oWknrU4=;";
+
+        private static void Initialize()
+        {
+        }
+        
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var serviceBusClient = new ServiceBusClient(ConnectionString);
+            var sender = serviceBusClient.CreateSender(ArjenTopicName);
+
+            using ServiceBusMessageBatch messageBatch = await sender.CreateMessageBatchAsync();
+
+            for (int i = 1; i <= 3; i++)
+                if (!messageBatch.TryAddMessage(new ServiceBusMessage($"Message {i}")))
+                    throw new Exception($"The message {i} is too large to fit in the batch.");
+
+            try
+            {
+                await sender.SendMessagesAsync(messageBatch);
+                Console.WriteLine($"A batch of {3} messages has been published to the topic.");
+            }
+            finally
+            {
+                // Calling DisposeAsync on client types is required to ensure that network
+                // resources and other unmanaged objects are properly cleaned up.
+                await sender.DisposeAsync();
+                await serviceBusClient.DisposeAsync();
+            }
+        }
+
+        static async Task MessageHandler(ProcessMessageEventArgs args)
+        {
+
+        }
+        static async Task ErrorHandler(ProcessErrorEventArgs args)
+        {
+
+        }
+    }
+}
